07c3f46aa113db5c21b6376170e8359c60eb3ce5
afranio caires <afraniomcaires@gmail.com>
2024-08-26 21:26:55 -0300

    fix

---
+++ app/Views/main_layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('recursos/bootstrap/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('recursos/css/estilo.css') ?>">
+    <title>ARA cursos</title>
+</head>
+
+<body>
+ <?=$this->include('nav')?>
+    <script src="<? base_url('recursos/bootstrap/bootstrap/boostrap.bundle.min.js') ?>"></script>
+
+</body>
+
+</html>
---
+++ app/Views/nav.php
@@ -0,0 +1,21 @@
+<nav class="container-fluid">
+    <div class="row align-items-center">
+        <div class="col p-3">
+            <a href="#">
+                <img src="<?= base_url('recursos/imagens/logo.png') ?>" alt="logo da Ara cursos">
+            </a>
+        </div>
+
+        <div class="">
+            <div>
+                <a href="">Início</a>
+            </div>
+            <div>
+                <a href="">Cursos</a>
+            </div>
+            <div>
+                <a href="">Onde estamos?</a>
+            </div>
+        </div>
+    </div>
+</nav>
